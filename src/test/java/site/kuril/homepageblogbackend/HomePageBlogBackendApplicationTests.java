c2820ca0277035a6db9ad72015d9af0a891d1fc5
kurilsang <3050882434@qq.com>
2025-09-08 20:01:21 +0800

    - 添加注释

--- src/test/java/site/kuril/homepageblogbackend/HomePageBlogBackendApplicationTests.java
+++ src/test/java/site/kuril/homepageblogbackend/HomePageBlogBackendApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HomePageBlogBackendApplicationTests {
-
+// 在这里可以添加测试类
     @Test
     void contextLoads() {
         System.out.println("test");
